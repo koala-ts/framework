a3d23e0e49fdb8218d2b478ada68c14fe96946a2
imdhemy <imdhemy@gmail.com>
2024-12-23 20:05:17 +0100

    chore: config build
    Update tsconfig.json

---
+++ tests/index.test.js
@@ -0,0 +1,6 @@
+import { expect, test } from 'vitest';
+import { main } from '../src/index.js';
+const message = await main();
+test('Example', () => {
+    expect(message).toBe('Howdy, Koala! 🐨');
+});
