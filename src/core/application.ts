84c8dde48e25c677cf10680d6633068459e58077
imdhemy <imdhemy@gmail.com>
2024-12-25 18:18:41 +0100

    wip: root directory

--- src/core/application.ts
+++ src/core/application.ts
@@ -1,10 +1,16 @@
 import { IApplication } from './types';
 import Koa, { Context } from 'koa';
 import Router from '@koa/router';
+import { get as trace } from 'stack-trace';
+import * as path from 'node:path';
+import { fileURLToPath } from 'url';
+import '../../playground/ExampleController';
 
 export function create(): IApplication {
     const app = new Koa();
     const router = new Router();
+
+    const rootDir = path.dirname(fileURLToPath((trace())[1].getFileName()));
 
     router.get('/', (ctx: Context) => {
         ctx.body = 'Hello, World!';
@@ -12,5 +18,4 @@ export function create(): IApplication {
 
     app.use(router.routes());
     return app;
-
 }
