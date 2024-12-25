4b1906c9767d52aebeb7470e380151950688f2a2
imdhemy <imdhemy@gmail.com>
2024-12-25 13:35:37 +0100

    wip: mvu

--- src/core/types.ts
+++ src/core/types.ts
@@ -3,3 +3,19 @@ import { Server } from 'http';
 export interface IApplication {
     listen(port?: number): Server;
 }
+
+export interface IModel {
+}
+
+export interface IMessage<Payload> {
+    type: string;
+    payload?: Payload;
+}
+
+export interface IUpdate<M extends IModel, Msg extends IMessage<unknown>> {
+    (model: M, message: Msg): M;
+}
+
+export interface IView<M extends IModel, Msg extends IMessage<unknown>> {
+    (model: M): Msg;
+}
