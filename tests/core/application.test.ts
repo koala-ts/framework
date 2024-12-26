6392f681bc3741a61602d162831e09bf12716826
imdhemy <imdhemy@gmail.com>
2024-12-26 13:42:25 +0100

    chore: improve test coverage

--- tests/core/application.test.ts
+++ tests/core/application.test.ts
@@ -1,8 +1,15 @@
 import { describe, expect, test } from 'vitest';
 import { create } from '../../src/core/application';
 import Koa from 'koa';
+import { Route } from '../../src/core/route';
 
 describe('Application', () => {
+    class FooController {
+        @Route('any', '/foo')
+        bar() {
+        }
+    }
+
     test('it creates application instance', () => {
         const app = create();
 
