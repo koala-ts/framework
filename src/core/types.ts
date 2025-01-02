26d6ad3c3256517f262622e342b3b2a8ed72d6f4
Dhemy <imdhemy@gmail.com>
2025-01-02 20:31:34 +0100

    feat(request): Access request parameters (#35)
    * chore: remove unused type
    
    * chore: add `IResponse` interface
    
    * Revert "chore: remove unused type"
    
    This reverts commit db5d05ce1039177ed640c5fd0e3629ac8277f7d9.
    
    * chore: add state and scope types
    
    * chore: code refactoring
    
    * feat(request): access request params

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,70 +1,59 @@
-import { IncomingMessage, Server, ServerResponse } from 'http';
-import { Middleware } from '@koa/router';
-import { Context, Next, Request, } from 'koa';
-import { Http2ServerRequest, Http2ServerResponse } from 'http2';
+import Koa, { Context, DefaultState, Next, Request, Response } from 'koa';
 
-export interface IApplication {
-    listen(port?: number): Server;
+export type IApplication<StateT = IState, ScopeT = IScope> = Koa<StateT, ScopeT>;
 
-    callback(): (req: IncomingMessage | Http2ServerRequest, res: ServerResponse | Http2ServerResponse) => Promise<void>;
-}
-
-export interface IRequest extends Request {
-    body?: { [key: string]: any };
-}
-
-export interface IHttpRequest extends IRequest {
-    body: { [key: string]: any };
+export interface IState extends DefaultState {
 }
 
 export interface IScope extends Context {
     request: IRequest | IHttpRequest;
+    response: IResponse;
 }
 
-export interface IMiddleware extends Middleware {
+export interface IRequest extends Request {
+    body?: Record<string, any>;
+    params: IScope['params'];
+}
+
+export interface IHttpRequest extends IRequest {
+    body: Record<string, any>;
+}
+
+export interface IResponse extends Response {
+}
+
+export interface IMiddleware {
     (scope: IScope, next: INext): Promise<INext>;
 }
 
 export interface INext extends Next {
 }
 
-
-export interface Handler extends Middleware {
+export interface Handler extends IMiddleware {
 }
 
 export type THttpMethod =
-    'get'
-    | 'post'
-    | 'put'
-    | 'patch'
-    | 'delete'
-    | 'options'
-    | 'head'
-    | 'any'
-    | 'all'
-    | 'GET'
-    | 'POST'
-    | 'PUT'
-    | 'PATCH'
-    | 'DELETE'
-    | 'OPTIONS'
-    | 'HEAD'
-    | 'ANY'
-    | 'ALL';
+    | 'get' | 'post' | 'put' | 'patch' | 'delete'
+    | 'options' | 'head' | 'any' | 'all'
+    | 'GET' | 'POST' | 'PUT' | 'PATCH'
+    | 'DELETE' | 'OPTIONS' | 'HEAD'
+    | 'ANY' | 'ALL';
 
-export type TRouterMethod = 'get' | 'post' | 'put' | 'patch' | 'delete' | 'options' | 'head' | 'all';
+export type TRouterMethod =
+    | 'get' | 'post' | 'put' | 'patch'
+    | 'delete' | 'options' | 'head' | 'all';
 
 export interface IRouteMetadata {
     path: string;
     methods: TRouterMethod[];
     handler: Handler;
     parseBody: boolean;
-    middleware: Middleware[];
+    middleware: IMiddleware[];
 }
 
 export interface IRouteOptions {
     path: string;
     method: THttpMethod | THttpMethod[];
     parseBody?: boolean;
-    middleware?: Middleware[];
+    middleware?: IMiddleware[];
 }
