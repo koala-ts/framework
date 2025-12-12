57dc4c861e9baff9541a8c99168f88d92b7fff68
Dhemy <imdhemy@gmail.com>
2025-12-13 00:43:45 +0100

    chore(ci): allow using any

--- src/Event/types.ts
+++ src/Event/types.ts
@@ -9,4 +9,5 @@ export interface EventBusStorage {
   scope: HttpScope;
 }
 
-export type EventSubscriber = (...args: never[]) => void;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type EventSubscriber = (...args: any[]) => void;
