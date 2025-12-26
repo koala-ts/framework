4a38eb6f216935f93ee9c12441589c06aa5212f0
Dhemy <imdhemy@gmail.com>
2025-12-26 19:57:42 +0100

    feat(node): use NodeJs v24 (#87)
    * feat(node): update Node.js version to 24 and enforce engine requirements
    
    * chore(package-lock): remove unnecessary peer dependencies
    
    * fix(types): enhance HttpRequest body type to support JsonValue

--- src/Http/Request/types.ts
+++ src/Http/Request/types.ts
@@ -1,11 +1,12 @@
 import type { File } from 'formidable';
 import type { Request } from 'koa';
+import type { JsonValue } from 'type-fest';
 
 export type UploadedFile = File;
 export type UploadedFilesMap = Record<string, UploadedFile | UploadedFile[]>;
 
 export interface HttpRequest extends Request {
-  body?: Record<string, unknown>;
+  body?: Record<string, unknown> & JsonValue;
   files?: UploadedFilesMap;
   params: Record<string, unknown>;
 }
