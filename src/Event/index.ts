bf7913238107d397ee790bf08d86d3b936a5bbea
Dhemy <imdhemy@gmail.com>
2025-12-28 00:16:53 +0100

    chore(bc): export additional types for event bus middleware

--- src/Event/index.ts
+++ src/Event/index.ts
@@ -1,5 +1,12 @@
 /**
- * @deprecated use `httpKernel` from `@/Kernel/HttpKernel` instead
- * {@see httpKernel}
+ * @deprecated This module is deprecated in favor of using the Kernel module.
  */
 export { httpKernel as eventBusMiddleware } from '@/Kernel/HttpKernel';
+export type {
+  KernelStorage as EventBusStorage,
+  EventEmitter,
+  EventSubscriber,
+  useEmit,
+  useResponse,
+  useRequest,
+} from '@/Kernel';
