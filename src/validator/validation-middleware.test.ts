e0de1974e1e78b157926a040d9a960794c0c3e7d
Eman Fateen <emanfateen@gmail.com>
2026-04-28 11:23:55 +0300

    chore: refactor second test case

--- src/validator/validation-middleware.test.ts
+++ src/validator/validation-middleware.test.ts
@@ -1,7 +1,5 @@
 import { HttpMiddleware, type HttpScope } from '@/Http';
-import { builtInConstraints } from '@/validator/constraints';
 import { flattenViolations } from '@/validator/flatten-violations';
-import { createValidator } from '@/validator/validator';
 import { describe, expect, test, vi } from 'vitest';
 import { validationMiddleware } from './validation-middleware';
 import { Validator } from '@/validator/types';
@@ -9,9 +7,9 @@ import { Validator } from '@/validator/types';
 describe('Validation middleware', () => {
   test('continues to the next middleware when no constrains are violated', async () => {
     const validate: Validator = vi.fn().mockReturnValue({});
-    const middleware: HttpMiddleware = validationMiddleware(validate, flattenViolations)({ username: ['notBlank'] });
+    const middleware: HttpMiddleware = validationMiddleware(validate, flattenViolations)({ name: ['notBlank'] });
     const scope = {
-      request: { body: { username: 'koala-user-name' } },
+      request: { body: { name: 'koala-example-name' } },
       response: { status: 200 },
     } as unknown as HttpScope;
     const next = vi.fn();
@@ -23,17 +21,27 @@ describe('Validation middleware', () => {
     expect(next).toHaveBeenCalledTimes(1);
   });
 
-  test('validates an empty payload when the request body is missing', async () => {
-    const validate = createValidator({ constraints: builtInConstraints });
-    const createMiddleware = validationMiddleware(validate, flattenViolations);
-    const middleware = createMiddleware({ username: ['notBlank'] });
-    const scope = { request: {}, response: {} } as unknown as HttpScope;
+  test('should create a response if constrains are violated', async () => {
+    const violations = [
+      {
+        path: 'name',
+        message: 'This value should not be blank.',
+        constraint: 'notBlank',
+        value: '',
+      },
+    ];
+    const validate: Validator = vi.fn().mockReturnValue(violations);
+    const middleware: HttpMiddleware = validationMiddleware(validate, flattenViolations)({ name: ['notBlank'] });
+    const scope = {
+      request: { body: { name: '' } },
+      response: { status: 404, body: {} },
+    } as unknown as HttpScope;
     const next = vi.fn();
 
     await middleware(scope, next);
 
     expect(scope.response.status).toBe(400);
-    expect(scope.response.body).toEqual({ errors: { username: ['This value should not be blank.'] } });
+    expect(scope.response.body).toEqual({ errors: { name: ['This value should not be blank.'] } });
     expect(next).not.toHaveBeenCalled();
   });
 });
