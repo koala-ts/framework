f6b323711010901ed1525cb9957e082a22106293
Eman Fateen <emanfateen@gmail.com>
2026-05-02 07:23:33 +0300

    chore: export createValidationMiddleware from the  index file

--- src/validator/index.ts
+++ src/validator/index.ts
@@ -2,3 +2,4 @@ export * from './validator';
 export * from './types';
 export * from './constraints';
 export * from './flatten-violations';
+export { createValidationMiddleware } from './create-validation-middleware';
