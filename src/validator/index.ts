4a2a8b10edbd7377755b3c125e4c840703926b41
Eman Fateen <emanfateen@gmail.com>
2026-05-14 14:04:34 +0300

    chore: move create-validation-middleware

--- src/validator/index.ts
+++ src/validator/index.ts
@@ -2,4 +2,4 @@ export * from '@/validator/factory/create-validator';
 export * from './types';
 export * from './constraints';
 export * from './flatten-violations';
-export { createValidationMiddleware } from './create-validation-middleware';
+export { createValidationMiddleware } from './middleware/create-validation-middleware';
