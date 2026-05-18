01718be79f6bd7a80adc2736c0fcc0ba48cca33b
Eman Fateen <emanfateen@gmail.com>
2026-05-18 15:36:57 +0300

    chore: remove un-need code

--- src/validator/constraints/index.ts
+++ src/validator/constraints/index.ts
@@ -20,7 +20,6 @@ export const builtInConstraints = {
 export * from './string/email';
 export * from './string/slug';
 export * from './basic/not-blank';
-export { unique, type UniqueOptions } from './comparison/unique';
 export * from './basic/type';
 export { unique, type UniqueOptions } from './comparison/unique';
 export * from './basic/type';
