6fec7255e12f7eede319508913763bd74b54efdf
Eman Fateen <emanfateen@gmail.com>
2026-05-23 00:11:44 +0300

    feat(validator): add unique validator constraint (#179)
    * Add unique constraint passing case
    
    * Detect duplicate values in unique constraint
    
    * Support custom unique constraint messages
    
    * Cover optional unique constraint values
    
    * Validate non-array unique constraint values
    
    * Support unique constraint normalizers
    
    * Support unique field combinations
    
    * Register built-in unique constraint
    
    # Conflicts:
    #	src/validator/constraints/index.ts
    
    * chore: remove unneeded test
    
    * feat(validator): Add type validator constraint (#173)
    
    * feat: add passing type constraint
    
    * chore: cover type constraint violation
    
    * chore: support matching type constraint values
    
    * chore: support type constraint alternatives
    
    * chore: allow optional type constraint values
    
    * chore: support type constraint custom message
    
    * chore: format type constraint alternatives
    
    * chore: register type constraint
    
    # Conflicts:
    #	src/validator/constraints/index.ts
    
    * chore: fix code style
    
    * chore: use TypeOptions due to rebase
    
    * Simplify type constraint message formatting
    
    * chore: add separate test case for the undefined values.
    
    * chore: refactor getDefaultMessageWith method.
    
    * chore: add a list of AllowedTypes types
    
    * chore: code format
    
    * chore: remove redundant test case
    
    * chore: remove redundant check
    
    * chore: fix AllowedTypes
    
    * chore: inline options
    
    * chore: rename test case
    
    * chore: use one default message
    
    * chore: inline method
    
    * chore: rename AllowedType
    
    * chore: refactor  normalizedTypes
    
    * chore: remove the undefined type case from each
    
    * chore(validator): make constraint options generic
    
    * chore: align type constraint options
    
    * chore: code refactoring
    
    * chore: fix type mismatch
    
    ---------
    
    Co-authored-by: Dhemy <imdhemy@gmail.com>
    
    * Register built-in unique constraint
    
    # Conflicts:
    #	src/validator/constraints/index.ts
    
    * chore: remove un-need code
    
    * chore: code refactoring
    
    * chore: return violation if the value is not an array
    
    * chore: in case no fields
    
    * chore: refactor code
    
    * chore: refactor code
    
    * chore: refactor code
    
    * chore: refactor code
    
    * chore: cover new test case
    
    * chore: refactor test cases
    
    * chore: rafctor code
    
    * fix: adjust semantics and unit tests
    
    * chore: remove unnecessary type assertions
    
    ---------
    
    Co-authored-by: Dhemy <imdhemy@gmail.com>

--- src/validator/constraints/index.ts
+++ src/validator/constraints/index.ts
@@ -1,12 +1,16 @@
 import { notBlank } from '@/validator/constraints/basic/not-blank';
+import { type } from '@/validator/constraints/basic/type';
+import { unique } from '@/validator/constraints/comparison/unique';
 import { email } from '@/validator/constraints/string/email';
 import { slug } from '@/validator/constraints/string/slug';
-import { type } from '@/validator/constraints/basic/type';
 
 export const builtInConstraints = {
   // Basic
   notBlank,
   type,
+
+  // Comparison
+  unique,
 
   // String
   email,
@@ -17,4 +21,5 @@ export * from './string/email';
 export * from './string/slug';
 export * from './basic/not-blank';
 export * from './basic/type';
+export { unique, type UniqueOptions } from './comparison/unique';
 export * from './other/compound';
