88eb60a801e6da4db74e3c1d7f46d307a924eac7
Dhemy <imdhemy@gmail.com>
2026-05-23 17:08:09 +0200

    feat(validator): add `all` constrait validator (#180)
    * feat(validator): add `all` constraint validator
    
    * feat(validator): format bracketed paths in flattened violations
    
    * Update index.ts

--- src/validator/constraints/index.ts
+++ src/validator/constraints/index.ts
@@ -1,6 +1,7 @@
 import { notBlank } from '@/validator/constraints/basic/not-blank';
 import { type } from '@/validator/constraints/basic/type';
 import { unique } from '@/validator/constraints/comparison/unique';
+import { all } from '@/validator/constraints/other/all';
 import { email } from '@/validator/constraints/string/email';
 import { slug } from '@/validator/constraints/string/slug';
 
@@ -12,14 +13,29 @@ export const builtInConstraints = {
   // Comparison
   unique,
 
+  // Other
+  all,
+
   // String
   email,
   slug,
 };
 
-export * from './string/email';
-export * from './string/slug';
-export * from './basic/not-blank';
-export * from './basic/type';
+// ## Basic ##
+export { notBlank } from './basic/not-blank';
+export { type, type TypeOptions } from './basic/type';
+// ## Basic ##
+
+// ## Comparison ##
 export { unique, type UniqueOptions } from './comparison/unique';
-export * from './other/compound';
+// ## Comparison ##
+
+// ## String ##
+export { email } from './string/email';
+export { slug } from './string/slug';
+// ## String ##
+
+// ## Other ##
+export { all, type AllOptions } from './other/all';
+export { compound } from './other/compound';
+// ## Other ##
--- src/validator/flatten-violations.ts
+++ src/validator/flatten-violations.ts
@@ -1,11 +1,18 @@
 import { Violation } from '@/validator/violation';
 
 export function flattenViolations(violations: Violation[]): Record<string, string[]> {
-  const groupedViolations = Object.groupBy(violations, violation => violation.path) as Record<string, Violation[]>;
+  const groupedViolations = Object.groupBy(violations, violation => formatPath(violation.path)) as Record<
+    string,
+    Violation[]
+  >;
 
   const violationEntries: Iterable<readonly [PropertyKey, string[]]> = Object.entries(groupedViolations).map(
     ([field, violations]) => [field, violations.map(v => v.message)],
   );
 
   return Object.fromEntries(violationEntries);
+}
+
+function formatPath(path: string): string {
+  return path.replaceAll('[', '.').replaceAll(']', '');
 }
