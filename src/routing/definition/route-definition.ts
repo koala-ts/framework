8233bbef78edea43c7960da710e05503205c3276
Eman Fateen <emanfateen@gmail.com>
2026-05-31 19:52:34 +0300

    chore: fix importing

--- src/routing/definition/route-definition.ts
+++ src/routing/definition/route-definition.ts
@@ -1,8 +1,7 @@
-import type { HttpRequest } from '@/Http';
+import type { HttpRequest, HttpMiddleware } from '@/Http';
 import type { RouterMethod } from '@/routing/router-method';
 import type { Request } from 'koa';
 import type { KoaBodyMiddlewareOptions } from 'koa-body';
-import { HttpMiddleware } from '../../Http';
 
 export interface RouteDefinition<TRequest extends Request = HttpRequest> {
   name?: string;
