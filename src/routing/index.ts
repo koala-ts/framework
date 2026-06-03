23f2004371e851e74d2fb1234dacdcd19c4dfadb
Dhemy <imdhemy@gmail.com>
2026-06-03 20:55:08 +0200

    wip: trim public api

--- src/routing/index.ts
+++ src/routing/index.ts
@@ -2,8 +2,3 @@ export { Route } from './route';
 export { createPathFor } from './helpers/create-path-for';
 export { RouteGroup } from './helpers/route-group';
 export { Any, Delete, Get, Head, Options, Patch, Post, Put } from './helpers/http-verb-helpers';
-export type { RouteDeclaration } from './route';
-export type { RouteConfigOverlay, RouteGroupDefinition, RouteGroupOptions } from './helpers/route-group';
-export type { RouteSource } from './source/route-source';
-export type { HttpMethod } from './http-method';
-export type { RouteOptions } from './route-options';
