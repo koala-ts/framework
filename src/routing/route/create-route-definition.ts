8b9a355796fbbab5ffc7b6628bfd22b32f432894
Dhemy <imdhemy@gmail.com>
2026-06-04 11:23:06 +0200

    Update create-route-definition.ts

--- src/routing/route/create-route-definition.ts
+++ src/routing/route/create-route-definition.ts
@@ -5,15 +5,6 @@ import type { RouterMethod } from '@/routing/route/router-method.type';
 import type { Request } from 'koa';
 import type { RouteDefinition } from './route-definition.type';
 
-interface RouteProps<TRequest extends Request = HttpRequest> {
-  name?: string;
-  method: HttpMethod | HttpMethod[];
-  path: string;
-  handler: HttpMiddleware<TRequest>;
-  middleware?: HttpMiddleware<TRequest>[];
-  options?: RouteOptions;
-}
-
 function resolveRouteOptions(options: RouteOptions): Pick<RouteDefinition, 'parseBody' | 'bodyOptions'> {
   return {
     parseBody: options.parseBody ?? true,
@@ -36,6 +27,15 @@ function qualifyMethods(method: HttpMethod | HttpMethod[]): RouterMethod[] {
   });
 
   return qualifiedMethods.includes('all') ? ['all'] : [...new Set<RouterMethod>(qualifiedMethods)];
+}
+
+interface RouteProps<TRequest extends Request = HttpRequest> {
+  name?: string;
+  method: HttpMethod | HttpMethod[];
+  path: string;
+  handler: HttpMiddleware<TRequest>;
+  middleware?: HttpMiddleware<TRequest>[];
+  options?: RouteOptions;
 }
 
 export function createRouteDefinition<TRequest extends Request = HttpRequest>(
